8dca8e800bfa59491824e0ff3dfc7cd8e892fb57
Anuj kumar <807anuj@gamil.com>
2024-08-28 23:53:43 +0530

    add about shadowing

--- basic.js
+++ basic.js
@@ -1,117 +1,139 @@
-// console.log(x)
-// var x=23;
-// // console.log()
+// // console.log(x)
+// // var x=23;
+// // // console.log()
 
-// let y=23;
+// // let y=23;
 
-// const value=45
-// value=34;
-// console.log(value) hare give 
-const value=23;
-console.log(value);
+// // const value=45
+// // value=34;
+// // console.log(value) hare give 
+// const value=23;
+// console.log(value);
 
-// let x;
-// x=23
-// x=25;
+// // let x;
+// // x=23
+// // x=25;
 
-// {
-//     var x=23
+// // {
+// //     var x=23
 
-// }
-// console.log(x)
-// console.log(x);
-// let x=34;
-//   {
-//   var x=36;
-//   console.log(x)
-//     }
-// console.log(x)
+// // }
+// // console.log(x)
+// // console.log(x);
+// // let x=34;
+// //   {
+// //   var x=36;
+// //   console.log(x)
+// //     }
+// // console.log(x)
 
-// function add(){
-//     {
-//   console.log(x)
-//     }
-//    var x=55;
-// }
-// add()
-// a=4 
-// console.log(a)
-// var a=23;
-// // console.log(y);
-// console.log(b)
-// var b=34;
-// let c=34
-//   c="anujkumar";
-// console.log(c)
-// c=4;
+// // function add(){
+// //     {
+// //   console.log(x)
+// //     }
+// //    var x=55;
+// // }
+// // add()
+// // a=4 
+// // console.log(a)
+// // var a=23;
+// // // console.log(y);
+// // console.log(b)
+// // var b=34;
+// // let c=34
+// //   c="anujkumar";
+// // console.log(c)
+// // c=4;
 
 
 
-// let y=30
-// { let x=23
-//   {
+// // let y=30
+// // { let x=23
+// //   {
      
-//     {
+// //     {
  
-// {
-//   console.log(y)
-//   console.log(x)
-// }
-// var y=40
-// }
-// }
-// }
+// // {
+// //   console.log(y)
+// //   console.log(x)
+// // }
+// // var y=40
+// // }
+// // }
+// // }
  
-// let x=34
+// // let x=34
 
-// function A(){
-//    x=36
-//  B()
-//   function B(){
-//     c()
-//     function c(){
-//       console.log(x)
-//         var x=23;
-//         }
-//         // c();
-//   }
-//   // B();
-// }
-// console.log(x)
-// A();
-// console.log(ADD)
+// // function A(){
+// //    x=36
+// //  B()
+// //   function B(){
+// //     c()
+// //     function c(){
+// //       console.log(x)
+// //         var x=23;
+// //         }
+// //         // c();
+// //   }
+// //   // B();
+// // }
+// // console.log(x)
+// // A();
+// // console.log(ADD)
 
-// // function ADD(a,b){
+// // // function ADD(a,b){
+// // //   let x=23;
+// // //   console.log(x)
+// // // return a+b;
+// // // }
+// // var ADD=function(a,b){
 // //   let x=23;
 // //   console.log(x)
-// // return a+b;
+// //   return a+b;
 // // }
-// var ADD=function(a,b){
-//   let x=23;
-//   console.log(x)
-//   return a+b;
-// }
-// ADD=34;
-// console.log(ADD)
-// let  ADD=function(a,b){
-//     let x=23;
-//     console.log(x)
-//     return a+b;
-//   }
+// // ADD=34;
+// // console.log(ADD)
+// // let  ADD=function(a,b){
+// //     let x=23;
+// //     console.log(x)
+// //     return a+b;
+// //   }
   
-// var ADD=(a,b)=>{
-//     let x=23;
-//     console.log(x)
-//     return a+b;
-//   }
+// // var ADD=(a,b)=>{
+// //     let x=23;
+// //     console.log(x)
+// //     return a+b;
+// //   }
   
 
 
-   let x=(function(a,b){
-    let x=34;
+//    let x=(function(a,b){
+//     let x=34;
     
-    console.log(x);
-    console.log(a+b)
-    return a+b
-  })(3,4)
-  console.log(x)
+//     console.log(x);
+//     console.log(a+b)
+//     return a+b
+//   })(3,4)
+//   console.log(x)
+
+//  var x=23;
+// {
+ 
+//   let x=23
+// console.log(x)
+// }
+
+// console.log(x) 
+
+
+// var x=23;
+// {
+ 
+//   let x=23
+// console.log(x)
+// }
+
+// console.log(x) 
+
+
+
